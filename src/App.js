0673fb83e7e0651f14432b589fc4301fcde7f523
aofleejay <aofleejay@hotmail.com>
2018-11-05 18:22:45 +0700

    Try custom apollo-link, apollo-link-http

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import './App.css'
+
+const fetchPosts = gql`
+  query fetchPosts {
+    allPosts {
+      id
+      description
+      imageUrl
+    }
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Query query={fetchPosts}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...'
+          if (error) return 'Error...'
+        
+          return (
+            <div className="App">
+              {data.allPosts.map(post => (
+                <div key={post.id}>{post.description}</div>
+              ))}
+            </div>
+          )
+        }}
+      </Query>
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloClient } from 'apollo-client'
+import { ApolloProvider
+ } from 'react-apollo'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logLink = new ApolloLink((operation , forward) => {
+  console.log(`starting request for ${operation.operationName}`)
+  return forward(operation).map((data) => {
+    console.log(`ending request for ${operation.operationName}`)
+    return data
+  })
+})
+
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_SERVICE_ENDPOINT })
+
+const client = new ApolloClient({
+  link: ApolloLink.from([logLink, httpLink]),
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
