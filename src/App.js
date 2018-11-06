adf8a7a90b34ff4feed5b8e1a27c220e2a769429
aofleejay <aofleejay@hotmail.com>
2018-11-06 15:23:13 +0700

    Language switch using apollo link state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import gql from 'graphql-tag'
-import { Query } from 'react-apollo'
+import { Query, Mutation } from 'react-apollo'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css'
 
-const fetchPosts = gql`
+const FETCH_POSTS = gql`
   query fetchPosts {
     allPosts {
       id
@@ -13,25 +14,79 @@ const fetchPosts = gql`
   }
 `
 
-class App extends Component {
-  render() {
-    return (
-      <Query query={fetchPosts}>
-        {({ loading, error, data }) => {
-          if (loading) return 'Loading...'
-          if (error) return 'Error...'
-        
-          return (
-            <div className="App">
-              {data.allPosts.map(post => (
-                <div key={post.id}>{post.description}</div>
-              ))}
-            </div>
-          )
-        }}
-      </Query>
-    )
+const GET_LANGUAGE = gql`
+  query getLanguage {
+    language @client
   }
+`
+
+const SET_LANGUAGE = gql`
+  mutation getLanguage($language: String!) {
+    setLanguage(language: $language) @client
+  }
+`
+
+const Home = () => {
+  return (
+    <Query query={GET_LANGUAGE}>
+      {({ data: { language }, loading, error }) => {
+        if (loading) return 'Loading...'
+        if (error) return 'Error...'
+
+        return (
+          <Query query={FETCH_POSTS}>
+            {({ loading, error, data }) => {
+              if (loading) return 'Loading...'
+              if (error) return 'Error...'
+            
+              return (
+                <div className="App">
+                  {data.allPosts.map(post => (
+                    <div key={post.id}>{post.description}</div>
+                  ))}
+                  <Mutation mutation={SET_LANGUAGE}>
+                    {(setLanguage) => {
+                      return (
+                        <>
+                          <p>Current language: {language}</p>
+                          <button onClick={() => setLanguage({ variables: { language: language === 'en' ? 'th' : 'en' } })}>
+                            Change to {language === 'en' ? 'th' : 'en'}
+                          </button>
+                        </>
+                      )
+                    }}
+                  </Mutation>
+                </div>
+              )
+            }}
+          </Query>
+        )
+      }}
+    </Query>
+  )
+}
+
+const Foo = () => 'Foo'
+
+const App = () => {
+  return (
+    <Router>
+      <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/foo">Foo</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/foo" component={Foo} />
+      </>
+    </Router>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import { ApolloProvider
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloLink } from 'apollo-link'
+import { withClientState } from 'apollo-link-state'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+const cache = new InMemoryCache()
 
 const logLink = new ApolloLink((operation , forward) => {
   console.log(`starting request for ${operation.operationName}`)
@@ -18,11 +21,27 @@ const logLink = new ApolloLink((operation , forward) => {
   })
 })
 
+const stateLink = withClientState({
+  cache,
+  defaults: {
+    language: 'th',
+  },
+  resolvers: {
+    Mutation: {
+      setLanguage: (_, { language }, { cache }) => {
+        console.log('l', language)
+        cache.writeData({ data: { language } })
+        return null
+      }
+    }
+  },
+})
+
 const httpLink = new HttpLink({ uri: process.env.REACT_APP_SERVICE_ENDPOINT })
 
 const client = new ApolloClient({
-  link: ApolloLink.from([logLink, httpLink]),
-  cache: new InMemoryCache()
+  link: ApolloLink.from([logLink, stateLink, httpLink]),
+  cache,
 })
 
 ReactDOM.render(
